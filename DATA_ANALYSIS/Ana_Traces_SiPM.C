9d53c3f71160e3d17724835930e6dbb9c487b472
davidedepaoli <35498225+davidedepaoli@users.noreply.github.com>
2018-02-20 13:23:39 +0100

    Ana_Traces_SiPM.C

--- DATA_ANALYSIS/Ana_Traces_SiPM.C
+++ DATA_ANALYSIS/Ana_Traces_SiPM.C
@@ -4,25 +4,27 @@
 #include <sstream>
 #include <string>
 #include <vector>
-#include <TMath.h>
-#include <TH1D.h>
-#include <TH2D.h>
-#include <TCanvas.h>
-#include <TF1.h>
-#include <TLegend.h>
-#include <TStyle.h>
-#include <TGraphErrors.h>
-#include <TAxis.h>
-#include <TPad.h>
-#include <TLine.h>
-#include <stdlib.h>     //for using the function sleep
+#include "TMath.h"
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TCanvas.h"
+#include "TF1.h"
+#include "TLegend.h"
+#include "TStyle.h"
+#include "TGraphErrors.h"
+#include "TAxis.h"
+#include "TPad.h"
+#include "TLine.h"
 
-using namespace std;
-using namespace TMath;
-
-void LineParser(ifstream& file_to_parse, vector<string>& lines);
-
-void Parse(ifstream& file_to_parse,vector<vector<string> >& spreadsheet);
+//------------------------------------------------------------------------------
+//-------------------------------[   FUNCTIONS   ]------------------------------
+//------------------------------------------------------------------------------
+int DLED(int trace_lenght, int dleddt);
+int find_peak_fix_time(int mintp, int maxtp);
+void average_func(int trace_lenght);
+void show_trace(TCanvas* canv, double *x, double *y, int trace_lenght, double miny, double maxy, int mintp, int maxtp, bool line_bool, bool delete_bool);
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
 
 //------------------------------------------------------------------------------
@@ -44,79 +46,11 @@ int bins_Volt = 204;
 //------------------------------------------------------------------------------
 
 
-//===============================================================================
-int DLED(int trace_lenght, int dleddt){
-    int trace_DLED_lenght = trace_lenght - dleddt;
-    //CREATE TRACE DLED
-    trace_DLED = new double*[2];
-    for(int i = 0; i < 2; i++) {
-        trace_DLED[i] = new double[trace_DLED_lenght];
-    }
-    for(int i=0; i<trace_DLED_lenght; i++){
-        trace_DLED[0][i] = trace[0][i + dleddt];
-        trace_DLED[1][i] = trace[1][i + dleddt]-trace[1][i];
-    }
-    return trace_DLED_lenght;
-}
-
-//===============================================================================
-int find_peak_fix_time(int mintp, int maxtp){
-       
-    double max = -100;
-    int index=0;
-    for(int i=mintp; i<maxtp; i++){
-        if(trace_DLED[1][i]>max){
-            max=trace_DLED[1][i];
-            index=i;
-        }
-    }
-    //cout<<index<<endl;
-    return index;
-}
-
-//===============================================================================
-void average_func(int trace_lenght){
-    int i;
-    for (i=0; i<trace_lenght; i++){
-        trace_AVG[1][i] = trace_AVG[1][i] + trace[1][i];
-    }
-}
-
-//===============================================================================   
-void show_trace(TCanvas* canv, double *x, double *y, int trace_lenght, double miny, double maxy){
-    canv->cd();
-    for(int i=0; i<trace_lenght; i++){
-        x[i] = x[i]*TMath::Power(10,6);
-        y[i] = y[i]*TMath::Power(10,3);
-    }
-    TGraphErrors *graph = new TGraphErrors(trace_lenght,x,y,0,0);
-    graph->SetTitle("");
-    graph->GetXaxis()->SetTitle("Time (#mus)");
-    graph->GetYaxis()->SetTitle("Amplitude (mV)");
-    graph->GetYaxis()->SetTitleOffset(1.2);
-    graph->GetXaxis()->SetTitleOffset(1.2);
-    graph->GetYaxis()-> SetRangeUser(miny,maxy);
-    graph->Draw("ap");
-    canv->Update();
-    graph->Set(0);
-}
-
-//===============================================================================   
-void show_line(TCanvas *canv, double *x, int mintp, int maxtp, double miny, double maxy){
-    canv->cd();
-    TLine *lmin = new TLine (x[mintp], miny, x[mintp], maxy);
-    TLine *lmax = new TLine (x[maxtp], miny, x[maxtp], maxy);
-    lmin->SetLineColor(kBlue);
-    lmax->SetLineColor(kBlue);
-    lmin->Draw("same");
-    lmax->Draw("same");
-}
 
 
-
-//=============================================================================== 
-//  MAIN FUNCTION
-//===============================================================================   
+//------------------------------------------------------------------------------
+//----------------------------[   MAIN FUNCTION   ]-----------------------------
+//------------------------------------------------------------------------------   
 int Analysis(string file, int last_event_n){
     gROOT->Reset();
     
@@ -268,12 +202,10 @@ int Analysis(string file, int last_event_n){
         if(display){
             if(Agilent_MSO6054A) {miny=10; maxy=180;}
             if(Digitizer_CAEN)   {miny=-900;  maxy=-700;}
-            show_trace(c,trace[0], trace[1], trace_lenght,miny,maxy);
-            show_line(c, trace[0], mintp, maxtp,miny,maxy);
+            show_trace(c,trace[0], trace[1], trace_lenght,miny,maxy,mintp,maxtp,true,true);
             if(Agilent_MSO6054A) {miny=-30; maxy=90;}
             if(Digitizer_CAEN)   {miny=-30; maxy=90;}
-            show_trace(cDLED,trace_DLED[0], trace_DLED[1], trace_DLED_lenght, miny, maxy);
-            show_line(cDLED, trace[0], mintp, maxtp, miny, maxy);
+            show_trace(cDLED,trace_DLED[0], trace_DLED[1], trace_DLED_lenght, miny, maxy,mintp,maxtp,true,true);
             getchar();
         }
         
@@ -297,8 +229,7 @@ int Analysis(string file, int last_event_n){
         cAVG->SetGrid();
         if(Agilent_MSO6054A){miny=50; maxy=90;}
         if(Digitizer_CAEN)  {miny=-855; maxy=-700;}
-        show_trace(cAVG,trace_AVG[0], trace_AVG[1], trace_lenght, miny, maxy);
-        show_line(cAVG, trace_AVG[0], mintp, maxtp, miny, maxy);
+        show_trace(cAVG,trace_AVG[0], trace_AVG[1], trace_lenght, miny, maxy,mintp,maxtp,true,false);
     }
     cHist->cd();
     if(SetLogyHist) cHist->SetLogy();
@@ -310,3 +241,70 @@ int Analysis(string file, int last_event_n){
 }
 
 
+
+
+
+//------------------------------------------------------------------------------
+int DLED(int trace_lenght, int dleddt){
+    int trace_DLED_lenght = trace_lenght - dleddt;
+    //CREATE TRACE DLED
+    trace_DLED = new double*[2];
+    for(int i = 0; i < 2; i++) {
+        trace_DLED[i] = new double[trace_DLED_lenght];
+    }
+    for(int i=0; i<trace_DLED_lenght; i++){
+        trace_DLED[0][i] = trace[0][i + dleddt];
+        trace_DLED[1][i] = trace[1][i + dleddt]-trace[1][i];
+    }
+    return trace_DLED_lenght;
+}
+
+//------------------------------------------------------------------------------
+int find_peak_fix_time(int mintp, int maxtp){
+       
+    double max = -100;
+    int index=0;
+    for(int i=mintp; i<maxtp; i++){
+        if(trace_DLED[1][i]>max){
+            max=trace_DLED[1][i];
+            index=i;
+        }
+    }
+    //cout<<index<<endl;
+    return index;
+}
+
+//------------------------------------------------------------------------------
+void average_func(int trace_lenght){
+    int i;
+    for (i=0; i<trace_lenght; i++){
+        trace_AVG[1][i] = trace_AVG[1][i] + trace[1][i];
+    }
+}
+
+//------------------------------------------------------------------------------
+void show_trace(TCanvas* canv, double *x, double *y, int trace_lenght, double miny, double maxy, int mintp, int maxtp, bool line_bool, bool delete_bool){
+    canv->cd();
+    for(int i=0; i<trace_lenght; i++){
+        x[i] = x[i]*TMath::Power(10,6);
+        y[i] = y[i]*TMath::Power(10,3);
+    }
+    TGraphErrors *graph = new TGraphErrors(trace_lenght,x,y,0,0);
+    graph->SetTitle("");
+    graph->GetXaxis()->SetTitle("Time (#mus)");
+    graph->GetYaxis()->SetTitle("Amplitude (mV)");
+    graph->GetYaxis()->SetTitleOffset(1.2);
+    graph->GetXaxis()->SetTitleOffset(1.2);
+    graph->GetYaxis()-> SetRangeUser(miny,maxy);
+    graph->Draw("aplsame");
+    if(line_bool){
+        TLine *lmin = new TLine (x[mintp], miny, x[mintp], maxy);
+        TLine *lmax = new TLine (x[maxtp], miny, x[maxtp], maxy);
+        lmin->SetLineColor(kBlue);
+        lmax->SetLineColor(kBlue);
+        lmin->Draw("aplsame");
+        lmax->Draw("aplsame");
+    }
+    canv->Update();
+    if(delete_bool) graph->Set(0);
+}
