3f97e2910a18fec8007199ada544da17a63543b5
davidedepaoli <35498225+davidedepaoli@users.noreply.github.com>
2018-02-25 21:18:40 +0100

    GAIN-V_SiPM.py

--- GAIN-V/GAIN-V_SiPM.py
+++ GAIN-V/GAIN-V_SiPM.py
@@ -64,40 +64,62 @@ def error_propagation_lin_fit(optimizedParameters, pcovODR, nSiPM):
 
 def main(**kwargs):
     
-    #--------------
-    #SiPM 1 (HD3_2)
-    #--------------
+    #----------------------------------------------------
+    #---------------[   SiPM 1 (HD3_2)   ]---------------
+    #----------------------------------------------------
     nSiPM = 1
     HV_SiPM1 = np.array([34.00, 35.00, 36.00])
     err_HV_SiPM1 = np.array([0.01, 0.01, 0.01])
+    
     #from Waweform with 2000 points (at max sample rate) (20180212_1_DARK_34_02.txt, 20180212_1_DARK_35_01.txt, 20180212_1_DARK_36_01.txt):
-    GAIN_SiPM1 = np.array([0.012197229689097585, 0.014442865747665316, 0.01599250638039572]) 
-    err_GAIN_SiPM1 = np.array([0.00019212343238446072, 0.00024665495118529894, 7.956049599216814e-05])
+    #GAIN_SiPM1 = np.array([0.012197229689097585, 0.014442865747665316, 0.01599250638039572]) 
+    #err_GAIN_SiPM1 = np.array([0.00019212343238446072, 0.00024665495118529894, 7.956049599216814e-05])
+    
+    #from files 20180221_HD3-2_1_DARK_34_AS_2_01.txt, 20180221_HD3-2_1_DARK_35_AS_2_01.txt, 20180221_HD3-2_1_DARK_36_AS_2_01.txt considering 15000 windows of 1 musec each:
+    GAIN_SiPM1 = np.array([17.2326, 19.6901, 22.0214]) 
+    err_GAIN_SiPM1 = np.array([0.0213087, 0.0217961, 0.0219385])
+    
     optimizedParameters1, pcovODR1 = fit(HV_SiPM1, GAIN_SiPM1, err_HV_SiPM1, err_GAIN_SiPM1)
     error_propagation_lin_fit(optimizedParameters1, pcovODR1, nSiPM)
     
     
-    #--------------
-    #SiPM 2 (HD3_2)
-    #--------------
+    
+    
+    #----------------------------------------------------
+    #---------------[   SiPM 2 (HD3_2)   ]---------------
+    #----------------------------------------------------
     nSiPM = 2
     HV_SiPM2 = np.array([34.00, 35.00, 36.00])
     err_HV_SiPM2 = np.array([0.01, 0.01, 0.01])
+    
     #from Waweform with 2000 points (at max sample rate) (20180212_2_DARK_34_01.txt, 20180212_2_DARK_35_01.txt, 20180212_2_DARK_36_01.txt):
-    GAIN_SiPM2 = np.array([0.011410721379920264,0.0135010146806218,0.0154419724055663]) 
-    err_GAIN_SiPM2 = np.array([0.0003764632506002512,0.00037003304446492515,0.00014876147139717083])
+    #GAIN_SiPM2 = np.array([0.011410721379920264,0.0135010146806218,0.0154419724055663]) 
+    #err_GAIN_SiPM2 = np.array([0.0003764632506002512,0.00037003304446492515,0.00014876147139717083])
+    
+    #from files 20180221_HD3-2_2_DARK_34_AS_2_02.txt, 20180221_HD3-2_2_DARK_35_AS_2_02.txt, 20180221_HD3-2_2_DARK_36_AS_2_02.txt considering 15000 windows of 1 musec each:
+    GAIN_SiPM2 = np.array([16.4713 ,18.7194,21.102]) 
+    err_GAIN_SiPM2 = np.array([0.0235355,0.0249616,0.0280182])
+    
     optimizedParameters2, pcovODR2 = fit(HV_SiPM2, GAIN_SiPM2, err_HV_SiPM2, err_GAIN_SiPM2)
     error_propagation_lin_fit(optimizedParameters2, pcovODR2, nSiPM)
     
-    #--------------
-    #SiPM 3 (HD3_2)
-    #--------------
+    
+    
+    #----------------------------------------------------
+    #---------------[   SiPM 3 (HD3_2)   ]---------------
+    #----------------------------------------------------
     nSiPM = 3
     HV_SiPM3 = np.array([34.00, 35.00, 36.00])
     err_HV_SiPM3 = np.array([0.01, 0.01, 0.01])
+    
     #from Waweform with 2000 points (at max sample rate) (20180209_DARK_34_03.txt, 20180209_DARK_35_02.txt, 20180209_DARK_36_01.txt):
-    GAIN_SiPM3 = np.array([0.015223774484941218, 0.01757637806904217, 0.019537323488766054]) 
-    err_GAIN_SiPM3 = np.array([0.0002636386386728474, 0.00031677656441436237, 0.00019640674762372849])
+    #GAIN_SiPM3 = np.array([0.015223774484941218, 0.01757637806904217, 0.019537323488766054]) 
+    #err_GAIN_SiPM3 = np.array([0.0002636386386728474, 0.00031677656441436237, 0.00019640674762372849])
+    
+    #from files 20180221_HD3-2_1_DARK_34_AS_2_03.txt, 20180221_HD3-2_1_DARK_35_AS_2_03.txt, 20180221_HD3-2_1_DARK_36_AS_2_03.txt considering 15000 windows of 1 musec each:
+    GAIN_SiPM3 = np.array([17.6488,20.1882,22.7506]) 
+    err_GAIN_SiPM3 = np.array([ 0.0207118,0.0223561,0.0242696])
+    
     optimizedParameters3, pcovODR3 = fit(HV_SiPM3, GAIN_SiPM3, err_HV_SiPM3, err_GAIN_SiPM3)
     error_propagation_lin_fit(optimizedParameters3, pcovODR3, nSiPM)
     plt.show()
