9b0261ddf1d0216abf90cd8bf89fd236c395560a
davidedepaoli <35498225+davidedepaoli@users.noreply.github.com>
2018-03-06 16:27:18 +0100

    DLED.cxx

--- OTHER/DLED.cxx
+++ OTHER/DLED.cxx
@@ -1,3 +1,20 @@
+//DLED.cxx
+
+/********************************************************************************
+ *  DLED.cxx                                                                    *
+ *                                                                              *
+ *  Plots some examples of signals analyzed using the DLED techniques           *
+ *                                                                              *
+ *  To RUN:                                                                     *
+ *  $ root -l                                                                   *
+ *  root[0] .x DLED.cxx                                                         *
+ *                                                                              *
+ *                                                                              *
+ *  Davide Depaoli 2018                                                         *
+ *                                                                              *
+ ********************************************************************************/
+
+
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
@@ -100,24 +117,24 @@ void DLED(){
     }
     
     //y
-    TCanvas *c = new TCanvas("c","Original");
-    c->Divide(1,2);
+    TCanvas *DLED_Original = new TCanvas("DLED_Original","DLED_Original");
+    DLED_Original->Divide(1,2);
     
     TGraph* gr = new TGraph(tot,x,y);
     gr->SetTitle("Signal");
     gr->SetLineColor(kBlue);
-    c->cd(1);
+    DLED_Original->cd(1);
     gr->Draw("al");
     
     TGraph* gr_DLED = new TGraph(tot,x,y_DLED);
     gr_DLED->SetTitle("DLED");
     gr_DLED->SetLineColor(kRed);
-    c->cd(2);
+    DLED_Original->cd(2);
     gr_DLED->Draw("al");
     
     //ysum1
-    TCanvas *c1 = new TCanvas("c1","DLED_0_timeUp");
-    c1->Divide(1,2);
+    TCanvas *DLED_0_timeUp = new TCanvas("DLED_0_timeUp","DLED_0_timeUp");
+    DLED_0_timeUp->Divide(1,2);
     
     TGraph* gr1 = new TGraph(tot,x,y1);
     gr1->SetTitle("Signal");
@@ -126,7 +143,7 @@ void DLED(){
     TGraph* grsum1 = new TGraph(tot,x,ysum1);
     grsum1->SetTitle("Signal");
     grsum1->SetLineColor(kBlack);
-    c1->cd(1);
+    DLED_0_timeUp->cd(1);
     grsum1->Draw("al");
     gr->Draw("same");
     gr1->Draw("same");
@@ -134,12 +151,12 @@ void DLED(){
     TGraph* grsum1_DLED = new TGraph(tot,x,ysum1_DLED);
     grsum1_DLED->SetTitle("DLED");
     grsum1_DLED->SetLineColor(kRed);
-    c1->cd(2);
+    DLED_0_timeUp->cd(2);
     grsum1_DLED->Draw("al");
     
     //ysum2
-    TCanvas *c2 = new TCanvas("c2","DLED_timeUp");
-    c2->Divide(1,2);
+    TCanvas *DLED_timeUp = new TCanvas("DLED_timeUp","DLED_timeUp");
+    DLED_timeUp->Divide(1,2);
     
     TGraph* gr2 = new TGraph(tot,x,y2);
     gr2->SetTitle("Signal");
@@ -148,7 +165,7 @@ void DLED(){
     TGraph* grsum2 = new TGraph(tot,x,ysum2);
     grsum2->SetTitle("Signal");
     grsum2->SetLineColor(kBlack);
-    c2->cd(1);
+    DLED_timeUp->cd(1);
     grsum2->Draw("al");
     gr->Draw("same");
     gr2->Draw("same");
@@ -156,12 +173,12 @@ void DLED(){
     TGraph* grsum2_DLED = new TGraph(tot,x,ysum2_DLED);
     grsum2_DLED->SetTitle("DLED");
     grsum2_DLED->SetLineColor(kRed);
-    c2->cd(2);
+    DLED_timeUp->cd(2);
     grsum2_DLED->Draw("al");
     
     //ysum3
-    TCanvas *c3 = new TCanvas("c3","DLED_timeUp_2timeUp");
-    c3->Divide(1,2);
+    TCanvas *DLED_timeUp_2timeUp = new TCanvas("DLED_timeUp_2timeUp","DLED_timeUp_2timeUp");
+    DLED_timeUp_2timeUp->Divide(1,2);
     
     TGraph* gr3 = new TGraph(tot,x,y3);
     gr3->SetTitle("Signal");
@@ -170,7 +187,7 @@ void DLED(){
     TGraph* grsum3 = new TGraph(tot,x,ysum3);
     grsum3->SetTitle("Signal");
     grsum3->SetLineColor(kBlack);
-    c3->cd(1);
+    DLED_timeUp_2timeUp->cd(1);
     grsum3->Draw("al");
     gr->Draw("same");
     gr3->Draw("same");
@@ -178,13 +195,13 @@ void DLED(){
     TGraph* grsum3_DLED = new TGraph(tot,x,ysum3_DLED);
     grsum3_DLED->SetTitle("DLED");
     grsum3_DLED->SetLineColor(kRed);
-    c3->cd(2);
+    DLED_timeUp_2timeUp->cd(2);
     grsum3_DLED->Draw("al");
     
     
     //ysum4
-    TCanvas *c4 = new TCanvas("c4","DLED_2timeUp");
-    c4->Divide(1,2);
+    TCanvas *DLED_2timeUp = new TCanvas("DLED_2timeUp","DLED_2timeUp");
+    DLED_2timeUp->Divide(1,2);
     
     TGraph* gr4 = new TGraph(tot,x,y4);
     gr4->SetTitle("Signal");
@@ -193,7 +210,7 @@ void DLED(){
     TGraph* grsum4 = new TGraph(tot,x,ysum4);
     grsum4->SetTitle("Signal");
     grsum4->SetLineColor(kBlack);
-    c4->cd(1);
+    DLED_2timeUp->cd(1);
     grsum4->Draw("al");
     gr->Draw("same");
     gr4->Draw("same");
@@ -201,13 +218,13 @@ void DLED(){
     TGraph* grsum4_DLED = new TGraph(tot,x,ysum4_DLED);
     grsum4_DLED->SetTitle("DLED");
     grsum4_DLED->SetLineColor(kRed);
-    c4->cd(2);
+    DLED_2timeUp->cd(2);
     grsum4_DLED->Draw("al");
     
     
     //ysum5
-    TCanvas *c5 = new TCanvas("c5","DLED_2timeUp_inf");
-    c5->Divide(1,2);
+    TCanvas *DLED_2timeUp_inf = new TCanvas("DLED_2timeUp_inf","DLED_2timeUp_inf");
+    DLED_2timeUp_inf->Divide(1,2);
     
     TGraph* gr5 = new TGraph(tot,x,y5);
     gr5->SetTitle("Signal");
@@ -216,7 +233,7 @@ void DLED(){
     TGraph* grsum5 = new TGraph(tot,x,ysum5);
     grsum5->SetTitle("Signal");
     grsum5->SetLineColor(kBlack);
-    c5->cd(1);
+    DLED_2timeUp_inf->cd(1);
     grsum5->Draw("al");
     gr->Draw("same");
     gr5->Draw("same");
@@ -224,47 +241,48 @@ void DLED(){
     TGraph* grsum5_DLED = new TGraph(tot,x,ysum5_DLED);
     grsum5_DLED->SetTitle("DLED");
     grsum5_DLED->SetLineColor(kRed);
-    c5->cd(2);
+    DLED_2timeUp_inf->cd(2);
     grsum5_DLED->Draw("al");
     
     
-    gr->GetXaxis()->SetTitle("time (a.u.)");
-    gr1->GetXaxis()->SetTitle("time (a.u.)");
-    gr2->GetXaxis()->SetTitle("time (a.u.)");
-    gr3->GetXaxis()->SetTitle("time (a.u.)");
-    gr4->GetXaxis()->SetTitle("time (a.u.)");
-    gr5->GetXaxis()->SetTitle("time (a.u.)");
+    gr->GetXaxis()->SetTitle("Time (a.u.)");
+    gr1->GetXaxis()->SetTitle("Time (a.u.)");
+    gr2->GetXaxis()->SetTitle("Time (a.u.)");
+    gr3->GetXaxis()->SetTitle("Time (a.u.)");
+    gr4->GetXaxis()->SetTitle("Time (a.u.)");
+    gr5->GetXaxis()->SetTitle("Time (a.u.)");
     
-    grsum1->GetXaxis()->SetTitle("time (a.u.)");
-    grsum2->GetXaxis()->SetTitle("time (a.u.)");
-    grsum3->GetXaxis()->SetTitle("time (a.u.)");
-    grsum4->GetXaxis()->SetTitle("time (a.u.)");
-    grsum5->GetXaxis()->SetTitle("time (a.u.)");
+    grsum1->GetXaxis()->SetTitle("Time (a.u.)");
+    grsum2->GetXaxis()->SetTitle("Time (a.u.)");
+    grsum3->GetXaxis()->SetTitle("Time (a.u.)");
+    grsum4->GetXaxis()->SetTitle("Time (a.u.)");
+    grsum5->GetXaxis()->SetTitle("Time (a.u.)");
     
-    grsum1_DLED->GetXaxis()->SetTitle("time (a.u.)");
-    grsum2_DLED->GetXaxis()->SetTitle("time (a.u.)");
-    grsum3_DLED->GetXaxis()->SetTitle("time (a.u.)");
-    grsum4_DLED->GetXaxis()->SetTitle("time (a.u.)");
-    grsum5_DLED->GetXaxis()->SetTitle("time (a.u.)");
+    gr_DLED->GetXaxis()->SetTitle("Time (a.u.)");
+    grsum1_DLED->GetXaxis()->SetTitle("Time (a.u.)");
+    grsum2_DLED->GetXaxis()->SetTitle("Time (a.u.)");
+    grsum3_DLED->GetXaxis()->SetTitle("Time (a.u.)");
+    grsum4_DLED->GetXaxis()->SetTitle("Time (a.u.)");
+    grsum5_DLED->GetXaxis()->SetTitle("Time (a.u.)");
     
-    gr->GetYaxis()->SetTitle("Amplitude(a.u.)");
-    gr1->GetYaxis()->SetTitle("Amplitude(a.u.)");
-    gr2->GetYaxis()->SetTitle("Amplitude(a.u.)");
-    gr3->GetYaxis()->SetTitle("Amplitude(a.u.)");
-    gr4->GetYaxis()->SetTitle("Amplitude(a.u.)");
-    gr5->GetYaxis()->SetTitle("Amplitude(a.u.)");
+    gr->GetYaxis()->SetTitle("Amplitude (a.u.)");
+    gr1->GetYaxis()->SetTitle("Amplitude (a.u.)");
+    gr2->GetYaxis()->SetTitle("Amplitude (a.u.)");
+    gr3->GetYaxis()->SetTitle("Amplitude (a.u.)");
+    gr4->GetYaxis()->SetTitle("Amplitude (a.u.)");
+    gr5->GetYaxis()->SetTitle("Amplitude (a.u.)");
     
-    grsum1->GetYaxis()->SetTitle("Amplitude(a.u.)");
-    grsum2->GetYaxis()->SetTitle("Amplitude(a.u.)");
-    grsum3->GetYaxis()->SetTitle("Amplitude(a.u.)");
-    grsum4->GetYaxis()->SetTitle("Amplitude(a.u.)");
-    grsum5->GetYaxis()->SetTitle("Amplitude(a.u.)");
+    grsum1->GetYaxis()->SetTitle("Amplitude (a.u.)");
+    grsum2->GetYaxis()->SetTitle("Amplitude (a.u.)");
+    grsum3->GetYaxis()->SetTitle("Amplitude (a.u.)");
+    grsum4->GetYaxis()->SetTitle("Amplitude (a.u.)");
+    grsum5->GetYaxis()->SetTitle("Amplitude (a.u.)");
     
-    grsum1_DLED->GetYaxis()->SetTitle("Amplitude(a.u.)");
-    grsum2_DLED->GetYaxis()->SetTitle("Amplitude(a.u.)");
-    grsum3_DLED->GetYaxis()->SetTitle("Amplitude(a.u.)");
-    grsum4_DLED->GetYaxis()->SetTitle("Amplitude(a.u.)");
-    grsum5_DLED->GetYaxis()->SetTitle("Amplitude(a.u.)");
+    grsum1_DLED->GetYaxis()->SetTitle("Amplitude (a.u.)");
+    grsum2_DLED->GetYaxis()->SetTitle("Amplitude (a.u.)");
+    grsum3_DLED->GetYaxis()->SetTitle("Amplitude (a.u.)");
+    grsum4_DLED->GetYaxis()->SetTitle("Amplitude (a.u.)");
+    grsum5_DLED->GetYaxis()->SetTitle("Amplitude (a.u.)");
     
     grsum1->SetLineWidth(2);
     grsum2->SetLineWidth(2);
@@ -272,12 +290,27 @@ void DLED(){
     grsum4->SetLineWidth(2);
     grsum5->SetLineWidth(2);
     
-    c->Update();
-    c1->Update();
-    c2->Update();
-    c3->Update();
-    c4->Update();
-    c5->Update();
+    double miny = 0;
+    double maxy = timeUp + timeDown;
+    
+    grsum1->GetXaxis()->SetRangeUser(miny,maxy);
+    grsum2->GetXaxis()->SetRangeUser(miny,maxy);
+    grsum3->GetXaxis()->SetRangeUser(miny,maxy);
+    grsum4->GetXaxis()->SetRangeUser(miny,maxy);
+    grsum5->GetXaxis()->SetRangeUser(miny,maxy);
+    
+    grsum1_DLED->GetXaxis()->SetRangeUser(miny,maxy);
+    grsum2_DLED->GetXaxis()->SetRangeUser(miny,maxy);
+    grsum3_DLED->GetXaxis()->SetRangeUser(miny,maxy);
+    grsum4_DLED->GetXaxis()->SetRangeUser(miny,maxy);
+    grsum5_DLED->GetXaxis()->SetRangeUser(miny,maxy);
+    
+    DLED_Original->Update();
+    DLED_0_timeUp->Update();
+    DLED_timeUp->Update();
+    DLED_timeUp_2timeUp->Update();
+    DLED_2timeUp->Update();
+    DLED_2timeUp_inf->Update();
     
     
 }
