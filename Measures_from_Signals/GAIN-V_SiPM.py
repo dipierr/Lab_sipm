16720a5a4cb1b72fb9091cb0fda00f07ddf09327
davidedepaoli <davide.depaoli.94@gmail.com>
2018-08-06 16:15:58 +0200

    Minor Improvements

--- Measures_from_Signals/GAIN-V_SiPM.py
+++ Measures_from_Signals/GAIN-V_SiPM.py
@@ -4,16 +4,11 @@
 ---------------------------------------------------------------------------------
 |                                                                               |
 |   GAIN-V_SiPM.py                                                              |
-|                                                                               |
-|   File to be analyzed: tab separated, written as:                             |
-|   V (V)   I (muA) errI (muA)                                                  | 
-|   with NO header                                                              |
-|                                                                               |
 |   Finds Vbd of SiPM using GAIV vs V data                                      |
 |                                                                               |
 |   To run:                                                                     |
 |   $ python GAIN-V_SiPM.py                                                     |
-|                                                                               |     
+|                                                                               |
 |   Davide Depaoli 2018                                                         |
 |                                                                               |
 ---------------------------------------------------------------------------------
@@ -26,7 +21,6 @@ from scipy.odr import *
 import argparse
 
 
-__description__ = 'Producing the azimuth and zenith angle for sim_telarray'
 
 formatter = argparse.ArgumentDefaultsHelpFormatter
 PARSER = argparse.ArgumentParser(description=__description__, formatter_class=formatter)
@@ -36,7 +30,7 @@ PARSER.add_argument('-o', '--option', type=int, required=False, default=1, help=
 def line_fit(p,x):
     m, q = p
     return m*x + q
-    
+
 def fit(HV_SiPM, GAIN_SiPM, err_HV_SiPM, err_GAIN_SiPM):
     plt.figure()
     plt.xlabel('HV (V)', fontsize = 18)
@@ -47,7 +41,7 @@ def fit(HV_SiPM, GAIN_SiPM, err_HV_SiPM, err_GAIN_SiPM):
     data = RealData(HV_SiPM, GAIN_SiPM, sx=err_HV_SiPM, sy=err_GAIN_SiPM)
     odr = ODR(data, fit_model, beta0=[1.,1.])
     out = odr.run()
-    out.pprint() 
+    out.pprint()
     pcovODR = odr.output.cov_beta # Covariance Matrix
     optimizedParameters = odr.output.beta # fit parameters
     xfit = np.arange(25, 37,0.01)
@@ -57,99 +51,99 @@ def fit(HV_SiPM, GAIN_SiPM, err_HV_SiPM, err_GAIN_SiPM):
     return optimizedParameters, pcovODR
 
 def error_propagation_lin_fit(optimizedParameters, pcovODR, nSiPM):
-    #linear fit: 
+    #linear fit:
     #y = mx +q => @y=0: mx=-q => x=-q/m
     m=optimizedParameters[0]
     q=optimizedParameters[1]
-    
+
     Vbd = -q/m
-    
+
     #partial derivatives
     df_dm = q/(m**2)
     df_dq = -1/m
-    
+
     #sigma
     sigma = 0
     sigma = sigma + pcovODR[0,0]*df_dm*df_dm + pcovODR[0,1]*df_dm*df_dq
     sigma = sigma + pcovODR[1,0]*df_dq*df_dm + pcovODR[1,1]*df_dq*df_dq
-    
+
     sigma = sigma**0.5
-    
+
     print('---------------------------------------------------------')
     print('Vbd_SiPM'+str(nSiPM)+'\t'+str(Vbd)+' +- '+str(sigma))
     print('---------------------------------------------------------\n\n')
-    
+
 
 def main(**kwargs):
-    
+
     #----------------------------------------------------
     #---------------[   SiPM 1 (HD3_2)   ]---------------
     #----------------------------------------------------
     nSiPM = 1
     HV_SiPM1 = np.array([34.00, 35.00, 36.00])
     err_HV_SiPM1 = np.array([0.01, 0.01, 0.01])
-    
+
     #from Waweform with 2000 points (at max sample rate) (20180212_1_DARK_34_02.txt, 20180212_1_DARK_35_01.txt, 20180212_1_DARK_36_01.txt):
-    #GAIN_SiPM1 = np.array([0.012197229689097585, 0.014442865747665316, 0.01599250638039572]) 
+    #GAIN_SiPM1 = np.array([0.012197229689097585, 0.014442865747665316, 0.01599250638039572])
     #err_GAIN_SiPM1 = np.array([0.00019212343238446072, 0.00024665495118529894, 7.956049599216814e-05])
-    
+
     #from files 20180221_HD3-2_1_DARK_34_AS_2_01.txt, 20180221_HD3-2_1_DARK_35_AS_2_01.txt, 20180221_HD3-2_1_DARK_36_AS_2_01.txt considering 15000 windows of 1 musec each:
-    #GAIN_SiPM1 = np.array([17.1506, 19.6551, 21.9663]) 
+    #GAIN_SiPM1 = np.array([17.1506, 19.6551, 21.9663])
     #err_GAIN_SiPM1 = np.array([0.0209611, 0.0217898, 0.0217723])
-    
+
     #from files 20180305_HD3-2_1_DARK_34_AS_2_01, 20180305_HD3-2_1_DARK_35_AS_2_01, 20180305_HD3-2_1_DARK_36_AS_2_01
-    GAIN_SiPM1 = np.array([19.6574, 22.627, 24.9128]) 
+    GAIN_SiPM1 = np.array([19.6574, 22.627, 24.9128])
     err_GAIN_SiPM1 = np.array([0.00944929, 0.00953918, 0.0111212])
-    
+
     optimizedParameters1, pcovODR1 = fit(HV_SiPM1, GAIN_SiPM1, err_HV_SiPM1, err_GAIN_SiPM1)
     error_propagation_lin_fit(optimizedParameters1, pcovODR1, nSiPM)
-    
-    
-    
-    
+
+
+
+
     #----------------------------------------------------
     #---------------[   SiPM 2 (HD3_2)   ]---------------
     #----------------------------------------------------
     nSiPM = 2
     HV_SiPM2 = np.array([34.00, 35.00, 36.00])
     err_HV_SiPM2 = np.array([0.01, 0.01, 0.01])
-    
+
     #from Waweform with 2000 points (at max sample rate) (20180212_2_DARK_34_01.txt, 20180212_2_DARK_35_01.txt, 20180212_2_DARK_36_01.txt):
-    #GAIN_SiPM2 = np.array([0.011410721379920264,0.0135010146806218,0.0154419724055663]) 
+    #GAIN_SiPM2 = np.array([0.011410721379920264,0.0135010146806218,0.0154419724055663])
     #err_GAIN_SiPM2 = np.array([0.0003764632506002512,0.00037003304446492515,0.00014876147139717083])
-    
+
     #from files 20180221_HD3-2_2_DARK_34_AS_2_02.txt, 20180221_HD3-2_2_DARK_35_AS_2_02.txt, 20180221_HD3-2_2_DARK_36_AS_2_02.txt considering 15000 windows of 1 musec each:
-    #GAIN_SiPM2 = np.array([16.1435 ,18.5611,20.9333]) 
+    #GAIN_SiPM2 = np.array([16.1435 ,18.5611,20.9333])
     #err_GAIN_SiPM2 = np.array([0.0226831,0.0241206,0.0272634])
-    
+
     #from files 20180305_HD3-2_2_DARK_34_AS_2_01, 20180305_HD3-2_2_DARK_35_AS_2_01, 20180305_HD3-2_2_DARK_36_AS_2_01
-    GAIN_SiPM2 = np.array([18.4962, 20.97, 23.4698]) 
+    GAIN_SiPM2 = np.array([18.4962, 20.97, 23.4698])
     err_GAIN_SiPM2 = np.array([0.00943349, 0.00990902, 0.0104709])
-    
+
     optimizedParameters2, pcovODR2 = fit(HV_SiPM2, GAIN_SiPM2, err_HV_SiPM2, err_GAIN_SiPM2)
     error_propagation_lin_fit(optimizedParameters2, pcovODR2, nSiPM)
-    
-    
-    
+
+
+
     #----------------------------------------------------
     #---------------[   SiPM 3 (HD3_2)   ]---------------
     #----------------------------------------------------
     nSiPM = 3
     HV_SiPM3 = np.array([34.00, 35.00, 36.00])
     err_HV_SiPM3 = np.array([0.01, 0.01, 0.01])
-    
+
     #from Waweform with 2000 points (at max sample rate) (20180209_DARK_34_03.txt, 20180209_DARK_35_02.txt, 20180209_DARK_36_01.txt):
-    #GAIN_SiPM3 = np.array([0.015223774484941218, 0.01757637806904217, 0.019537323488766054]) 
+    #GAIN_SiPM3 = np.array([0.015223774484941218, 0.01757637806904217, 0.019537323488766054])
     #err_GAIN_SiPM3 = np.array([0.0002636386386728474, 0.00031677656441436237, 0.00019640674762372849])
-    
+
     #from files 20180221_HD3-2_1_DARK_34_AS_2_03.txt, 20180221_HD3-2_1_DARK_35_AS_2_03.txt, 20180221_HD3-2_1_DARK_36_AS_2_03.txt considering 15000 windows of 1 musec each:
-    #GAIN_SiPM3 = np.array([17.5343,20.0972,22.6212]) 
+    #GAIN_SiPM3 = np.array([17.5343,20.0972,22.6212])
     #err_GAIN_SiPM3 = np.array([ 0.0201935,0.022037,0.0237572])
-    
+
     #from files 20180305_HD3-2_3_DARK_34_AS_2_01, 20180305_HD3-2_3_DARK_35_AS_2_01, 20180305_HD3-2_3_DARK_36_AS_2_01
-    GAIN_SiPM3 = np.array([20.074, 22.8873, 25.5604]) 
+    GAIN_SiPM3 = np.array([20.074, 22.8873, 25.5604])
     err_GAIN_SiPM3 = np.array([0.00899452,0.00963387 , 0.0108095])
-    
+
     optimizedParameters3, pcovODR3 = fit(HV_SiPM3, GAIN_SiPM3, err_HV_SiPM3, err_GAIN_SiPM3)
     error_propagation_lin_fit(optimizedParameters3, pcovODR3, nSiPM)
     plt.show()
